74cbfcf5c06bb056e6c96d526f25d02395c47abe
mangx <771854332@qq.com>
2017-10-13 19:04:15 +0800

    1.0.0 release

--- model/db.js
+++ model/db.js
@@ -2,7 +2,7 @@ var mysql      = require('mysql');
 var pool  = mysql.createPool({
     host     : 'localhost',
     user     : 'root',
-    password: '771854332',
+    password: 'qiangge961025',
     database : 'bengi_blog'
 });
 

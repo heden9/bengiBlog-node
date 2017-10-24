6ab5b8b3178cce8b65f0911a0b53d87f912bc33b
w771854332 <771854332@qq.com>
2017-10-24 15:48:15 +0800

    config cache

--- app.js
+++ app.js
@@ -21,7 +21,11 @@ app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, 'public'), {
+  maxAge: 100000000,
+  etag: false,
+  lastModified: false
+}));
 
 // app.all('*', function(req, res){
   
